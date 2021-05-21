5a6e099f9403057ea81bf9af1111c7b8fbf8a8ff
heberhardt <hjve@h2tecnologia.com>
2021-05-21 15:40:43 -0300

    Check-in inicial do módulo ItemsApiConsume

---
+++ ItemsApiConsume/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using ItemsApiConsume.Services;
+using ItemsApiConsume.Interfaces;
+
+namespace ItemsApiConsume
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            //Define o endereço base das chamadas de api, para que não seja necessário ficar chamando o endereço completo toda vez.
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://itemsapi-h2tecnologia-com.umbler.net") });
+
+            //Aqui a gente faz a injeção da dependencia, em resumo:
+            //    Transient objects are always different; a new instance is provided to every controller and every service.
+            //    Scoped objects are the same within a request, but different across different requests.
+            //    Singleton objects are the same for every object and every request.
+            builder.Services.AddScoped<IItemsService, ItemsService>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ ItemsApiConsume/Services/ItemsService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using ItemsApiConsume.Interfaces;
+using ItemsApiConsume.Models;
+
+namespace ItemsApiConsume.Services
+{
+    public class ItemsService : IItemsService //Informa que a classe ItemService precisa ter tudo que a interface IItemsService tem.
+    {
+        private readonly HttpClient _http;
+
+        public ItemsService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<List<Item>> GetItems()
+        {
+            return await _http.GetFromJsonAsync<List<Item>>("items");
+        }
+
+
+        public async Task<Item> PostItem(Item item)
+        {
+            HttpResponseMessage response = await _http.PostAsJsonAsync("items", item);
+
+            return await response.Content.ReadFromJsonAsync<Item>();
+        }
+
+
+        public async Task<Item> UpdateItem(Item item)
+        {
+            HttpResponseMessage response = await _http.PutAsJsonAsync("items", item);
+
+            return await response.Content.ReadFromJsonAsync<Item>();
+        }
+
+        public async Task<Item> DeleteItem(Item item)
+        {
+            HttpResponseMessage response = await _http.DeleteAsync("items/" + item.Id, new CancellationToken());
+
+            return await response.Content.ReadFromJsonAsync<Item>();
+        }
+    }
+}
