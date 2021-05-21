45f2b1daeb249cd49f36989d43fbbc4fea5973ca
heberhardt <hjve@h2tecnologia.com>
2021-05-21 16:37:48 -0300

    Adicionado netlify.proto

---
+++ Interfaces/IItemsService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ItemsApiConsume.Models;
+
+namespace ItemsApiConsume.Interfaces
+{
+    //Essa interface define tudo que a classe filha dela precisa ter;
+    public interface IItemsService
+    {
+        Task<List<Item>> GetItems();
+        Task<Item> PostItem(Item item);
+        Task<Item> UpdateItem(Item item);
+        Task<Item> DeleteItem(Item item);
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ItemsApiConsume.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
